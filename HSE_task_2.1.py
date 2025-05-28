d0104f39a69d2613f3edcae74498824eaddfbd6b
Rasul <rasl16@ya.ru>
2025-05-28 17:16:22 +0500

    Даны два целых числа A и В. Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.

---
+++ HSE_task_2.1.py
@@ -0,0 +1,11 @@
+
+print("Введите 2 целых числа, они не должны быть равны")
+a = int(input("Введите число А: "))
+b = int(input("Введите число В: "))
+
+if a < b:
+    for i in range(a, b + 1):
+        print (i)
+else:
+    for i in range(a, b - 1, -1):
+        print(i)
