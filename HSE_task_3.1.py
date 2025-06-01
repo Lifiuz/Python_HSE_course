690c6d5f51358724b78c9a50bf2b7d20848d41d8
Rasul <rasl16@ya.ru>
2025-06-01 21:05:10 +0500

    Написать функцию вычисления площади прямоугольника. Входные параметры: ширина, высота. Выходной параметр: площадь

---
+++ HSE_task_3.1.py
@@ -0,0 +1,18 @@
+a = int(input("Введите длину: "))
+b = int(input("Введите ширину: "))
+def square_rectangle(dlina, shirina):
+    return a * b
+print(f"Площадь прямоугольника a*b = {square_rectangle(a, b)}")
+
+
+
+
+
+
+
+
+#rasul = lambda x,y: x + y
+#print(rasul(10, 20))
+#firuz = lambda a, b: a + b
+#print(firuz(30, 40))
+#sum_rasul = rasul + firuz
